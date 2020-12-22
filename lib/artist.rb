77aa7e55ec08f1030b7bb81ef3bedc4fd93a072c
Christopher Mrakovcic <cmrakovcic@aol.com>
2020-12-22 18:13:53 -0500

    commit tree cd33d7ffa35ef6119674fcf86b0a803f72952956

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,31 @@
+class Artist
+  @@all = []
+  attr_accessor :name, :artist, :genre
 
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(title, genre)
+    Song.new(title, self, genre)
+    self.songs.last
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    Song.all.map do |song|
+      song.genre
+    end
+  end
+  
+end
